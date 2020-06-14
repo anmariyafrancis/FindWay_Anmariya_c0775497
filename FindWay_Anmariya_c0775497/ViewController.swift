fb50e862837bf32ce614eabd03c58ad334ff43c0
anmariyafrancis <anmariya1915@gmail.com>
2020-06-14 19:49:48 -0400

    [ANMARIYA] initial commit

--- FindWay_Anmariya_c0775497/ViewController.swift
+++ FindWay_Anmariya_c0775497/ViewController.swift
@@ -7,14 +7,128 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var btnFindWay: UIButton!
+    @IBOutlet weak var segmentType: UISegmentedControl!
+    
+    var locationManager = CLLocationManager()
+    var aLat: CLLocationDegrees??
+    var aLon: CLLocationDegrees??
+    var location: CLLocation?
+    
+     @IBAction func indexChanged(_ sender: Any) {
+           
+            route()
+ }
+     @IBAction func findMyWay(_ sender: Any) {
+            route()
+  }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+       
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+        mapView.isZoomEnabled = false
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+        tap.numberOfTapsRequired = 2
+        mapView.addGestureRecognizer(tap)
     }
+    
+    
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+        {
+            
+            location = locations.first!
+            let coordinateRegion = MKCoordinateRegion(center: location!.coordinate, latitudinalMeters: 1000, longitudinalMeters:1000)
+            mapView.setRegion(coordinateRegion, animated: true)
+            locationManager.stopUpdatingLocation()
+        }
+    
+     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 3;
+        return renderer
+    }
+    
+    
+      @objc func doubleTapped(sender: UIGestureRecognizer)
+        {
+           
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(location: locationOnMap)
+        }
+    
+      func route()
+    {
+            self.mapView.removeOverlays(self.mapView.overlays)
+           
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!), addressDictionary: nil))
+        
+        if(aLat == nil || aLon == nil)
+        {
+                let alertController = UIAlertController(title: "Error", message:
+                "Destination Not Selected", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .default))
 
+                self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: aLat as! CLLocationDegrees, longitude: aLon as! CLLocationDegrees), addressDictionary: nil))
+            request.requestsAlternateRoutes = false
+        }
+       
+        switch segmentType.selectedSegmentIndex
+        {
+            case 0:
+                request.transportType = .walking
+            case 1:
+                request.transportType = .automobile
+            default:
+                break
+        }
+        let directions = MKDirections(request: request)
 
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+
+            for route in unwrappedResponse.routes {
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
+    }
+    
+    
+     func addAnnotation(location: CLLocationCoordinate2D)
+        {
+           
+            let oldAnnotations = self.mapView.annotations
+            self.mapView.removeAnnotations(oldAnnotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            aLat = annotation.coordinate.latitude
+            aLon = annotation.coordinate.longitude
+            annotation.title = "Destination"
+            annotation.subtitle = "Destination"
+            self.mapView.addAnnotation(annotation)
+        }
 }
+
 
