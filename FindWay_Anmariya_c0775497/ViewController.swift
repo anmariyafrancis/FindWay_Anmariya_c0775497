dce35246c06c30ef6646abccd6c0c1201141d2bc
anmariyafrancis <anmariya1915@gmail.com>
2020-06-14 20:33:45 -0400

    [anmariya] removed errors in view controller

--- FindWay_Anmariya_c0775497/ViewController.swift
+++ FindWay_Anmariya_c0775497/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var segmentType: UISegmentedControl!
     
+    let latitude: CLLocationDegrees = 43.64
+    let longitude: CLLocationDegrees = -79.38
+    
     var locationManager = CLLocationManager()
     var aLat: CLLocationDegrees??
     var aLon: CLLocationDegrees??
@@ -37,6 +40,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
+        
         locationManager.startUpdatingLocation()
         
         mapView.showsUserLocation = true
@@ -51,12 +55,34 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
         {
             
-            location = locations.first!
-            let coordinateRegion = MKCoordinateRegion(center: location!.coordinate, latitudinalMeters: 1000, longitudinalMeters:1000)
-            mapView.setRegion(coordinateRegion, animated: true)
-            locationManager.stopUpdatingLocation()
+           // location = locations.first!
+            //
+            
+            let userLocation = locations[0]
+            
+            let latitude = userLocation.coordinate.latitude
+            let longitude = userLocation.coordinate.longitude
+    
+            displayLocation(latitude: latitude, longitude: longitude, title: "Your Location", subtitle: "you are here")
+            //locationManager.stopUpdatingLocation()
         }
     
+        func displayLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String, subtitle: String) {
+               // 2 - define delta latitude and delta longitude for the span
+               let latDelta: CLLocationDegrees = 0.05
+               let lngDelta: CLLocationDegrees = 0.05
+               
+               // 3 - creating the span and location coordinate and finally the region
+               let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+               let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+               let region = MKCoordinateRegion(center: location, span: span)
+               
+               // 4 - set region for the map
+            
+               mapView.setRegion(coordinateRegion, animated: true)
+               
+        
+           }
     
     
      func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
